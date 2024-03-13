dc5da61c50bbabc4c80d109bf1d4bc7a44a43506
Cguru <z244300@gmail.com>
2024-03-13 20:32:29 +0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
+#Пересборка и старт контейнера:
+#docker build -t my-php-app .
+#docker run -p 8282:8282 -v $(pwd):/var/www/html  my-php-app
+
+
 # Используем образ PHP 8.3 с поддержкой CLI и FPM
 FROM php:8.3-cli
 
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <div>
     <h2>TestDeployOff</h2>
     <img src="https://via.placeholder.com/150" alt="Placeholder Image">
+    <img src="img/big.jpg" alt="Placeholder Image">
     <p><a href="page2.php">Перейти на страницу 2</a></p>
     <p><a href="page3.php">Page 3</a></p>
     <p><a href="page4.php">Page 4</a></p>
